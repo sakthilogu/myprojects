80ac65d7482c32ba304371c73eda4e5a6a67796c
sakthi <sakthi.irtt@gmail.com>
2018-08-01 13:38:36 +0530

    added jar

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Dell\workspace\authenticate
+java -cp  C:\Users\Dell\workspace\authenticate\target\test-classes\*;C:\Users\Dell\workspace\authenticate\lib\* org.testng.TestNG  testng.xml
